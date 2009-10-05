e9a559873b13179e783fd7dcc030744bd5637993
Jon Gaudette <jon@TestingServer.(none)>
2009-10-05 00:43:54 +0000

    changing decimal migration

---
+++ db/migrate/20091005004103_change_decimals.rb
@@ -0,0 +1,11 @@
+class ChangeDecimals < ActiveRecord::Migration
+  def self.up
+    remove_column :weights, :weight
+    add_column :weights, :weight, :decimal, :precision=>12 :scale=>2
+  end
+
+  def self.down
+    remove_column :weights, :weight
+    add_column :weights, :weight, :decimal
+  end
+end
