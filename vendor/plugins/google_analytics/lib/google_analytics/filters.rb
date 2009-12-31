5bff3f986f5076b291db74c153e79c92079e772a
Jon Gaudette <jon@TestingServer.(none)>
2009-12-31 14:30:47 +0000

    removing google analytics plugin in favor of homebrew partial

--- vendor/plugins/google_analytics/lib/google_analytics/filters.rb
+++
@@ -1,7 +0,0 @@
-module GoogleAnalytics    
-  module Filters    
-    def google_analytics(*args)
-      GoogleAnalytics.add_urchin_tracker(*args)
-    end
-  end
-end
--- vendor/plugins/google_analytics/lib/google_analytics/macro.rb
+++
@@ -1,7 +0,0 @@
-module GoogleAnalytics
-  class Macro < FilteredColumn::Macros::Base
-    def self.filter(attributes, link = '', text = '')
-      GoogleAnalytics.add_urchin_tracker(link, attributes[:token])
-    end
-  end
-end
