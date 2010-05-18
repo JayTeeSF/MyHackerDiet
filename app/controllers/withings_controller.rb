9a319d14cd3e671491f3555bafff881792c3127c
Jon <jon@digital-drip.com>
2010-05-18 09:28:38 -0400

    bug fix: message using wrong variable name

--- app/controllers/withings_controller.rb
+++ app/controllers/withings_controller.rb
@@ -5,6 +5,6 @@ class WithingsController < ApplicationController
     @withings_userid = params[:userid]
     @withings_startdate = params[:startdate]
     @withings_enddate = params[:enddate]
-    Emailer.deliver_contact('jon@digital-drip.com', "MyHackerDiet Event for User #{@withings_user}", "#{@withings_userid} - #{@withings_startdate}")
+    Emailer.deliver_contact('jon@digital-drip.com', "MyHackerDiet Event for User #{@withings_userid}", "#{@withings_userid} - #{@withings_startdate}")
   end
 end
