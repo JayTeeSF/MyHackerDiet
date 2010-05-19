b5a4d197251c4553b6a5f078ac3621fd89c6c98b
Jon <jon@digital-drip.com>
2010-05-19 12:13:25 -0400

    Add rmr/bmi/tdee, fix weight and person relationship

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,3 +1,16 @@
 class Weight < ActiveRecord::Base
-  has_one :person
+  belongs_to :person
+
+
+  def bmi
+    return ((avg_weight * 703) / (person.height**2)).round(2)
+  end
+
+  def rmr
+    return ((9.99 * (avg_weight/2.2)) + (6.25 * (person.height * 2.54)) - (4.92 * person.age) + 5).round(2)
+  end
+
+  def tdee
+    return (rmr * 1.2).round(2)
+  end
 end
