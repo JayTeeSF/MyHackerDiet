2b78be8d65a45519f4aff31c7ff273e129617c5e
Jon <jon@digital-drip.com>
2010-05-19 12:19:09 -0400

    add nil condition check to calculations

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -3,10 +3,16 @@ class Weight < ActiveRecord::Base
 
 
   def bmi
+    if person.height == nil
+      return 0
+
     return ((avg_weight * 703) / (person.height**2)).round(2)
   end
 
   def rmr
+    if person.height == nil || person.age == nil
+      return 0
+
     return ((9.99 * (avg_weight/2.2)) + (6.25 * (person.height * 2.54)) - (4.92 * person.age) + 5).round(2)
   end
 
