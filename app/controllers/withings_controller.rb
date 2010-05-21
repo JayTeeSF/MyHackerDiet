db15718991fa6520fb2acfd2ed4b0d34fbfc9766
Jon <jon@digital-drip.com>
2010-05-20 20:53:35 -0400

    add enddate to withings email, demote variables

--- app/controllers/withings_controller.rb
+++ app/controllers/withings_controller.rb
@@ -2,9 +2,9 @@ class WithingsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def log
-    @withings_userid = params[:userid]
-    @withings_startdate = params[:startdate]
-    @withings_enddate = params[:enddate]
-    Emailer.deliver_contact('jon@digital-drip.com', "MyHackerDiet Event for User #{@withings_userid}", "#{@withings_userid} - #{@withings_startdate}")
+    withings_userid = params[:userid]
+    withings_startdate = params[:startdate]
+    withings_enddate = params[:enddate]
+    Emailer.deliver_contact('jondigital-drip.com', "MyHackerDiet Event for User #{withings_userid}", "#{withings_userid} - #{withings_startdate} -> #{withings_enddate}")
   end
 end
