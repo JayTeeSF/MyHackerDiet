84a94d69a51cddb09a3902ef8d85cc9692c245a3
Jon <jon@digital-drip.com>
2010-05-23 20:27:25 -0400

    Update capistrano for passenger

--- Capfile
+++ Capfile
@@ -1,22 +1,4 @@
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
 Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
-                               load 'config/deploy'
 
-namespace :deploy do
-  %w(start stop restart).each do |action| 
-    desc "#{action} the Thin processes"  
-    task action.to_sym do
-      find_and_execute_task("thin:#{action}")
-    end
-  end 
-end
-
-namespace :thin do  
-  %w(start stop restart).each do |action| 
-    desc "#{action} the app's Thin Cluster"  
-    task action.to_sym, :roles => :app do  
-      run "thin #{action} -c #{deploy_to}/current -C #{deploy_to}/current/config/thin.yml" 
-    end
-  end
-end
-
+load 'config/deploy' # remove this line to skip loading any of the default tasks
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,26 @@
 set :application, "mhd"
-#set :domain, "myhackerdiet.com"
 set :repository,  "jon@horder.digital-drip.com:/home/jon/git/myhackerdiet.git"
-
-# If you have previously been relying upon the code to start, stop 
-# and restart your mongrel application, or if you rely on the database
-# migration code, please uncomment the lines you require below
-
-# If you are deploying a rails app you probably need these:
-
-#load 'ext/rails-database-migrations.rb'
-#load 'ext/rails-shared-directories.rb'
-
-# There are also new utility libaries shipped with the core these 
-# include the following, please see individual files for more
-# documentation, or run `cap -vT` with the following lines commented
-# out to see what they make available.
-
-# load 'ext/spinner.rb'              # Designed for use with script/spin
-# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
-# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set :deploy_to, "/srv/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
 set :scm, :git
-set :user, "jon"
-set :branch, "master"
-# see a full list by running "gem contents capistrano | grep 'scm/'"
+set :user, 'jon'
+set :branch, 'master'
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :app, "myhackerdiet.com"
-role :web, "myhackerdiet.com"
-role :db, "myhackerdiet.com"
+role :web, "myhackerdiet.com"                          # Your HTTP server, Apache/etc
+role :app, "myhackerdiet.com"                          # This may be the same as your `Web` server
+role :db,  "myhackerdiet.com", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
-#set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
 
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
