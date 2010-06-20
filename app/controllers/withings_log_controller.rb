52a681dcb88a9c48d50a78593ee66c6703a7cdb5
Jon <jon@digital-drip.com>
2010-06-20 12:44:15 -0400

    Sort in database not in view

--- app/controllers/withings_log_controller.rb
+++ app/controllers/withings_log_controller.rb
@@ -1,7 +1,7 @@
 class WithingsLogController < ApplicationController
   def index
-    @logs = WithingsLog.find_all_by_userid(@user.withings_uid, :order => 'created_at ASC')
-    @withings_logs = Withings.find_all_by_userid(@user.withings_uid, :order => 'created_at ASC')
+    @logs = WithingsLog.find_all_by_userid(@user.withings_uid, :order => 'sdate DESC')
+    @withings_logs = Withings.find_all_by_userid(@user.withings_uid, :order => 'rec_date DESC')
 
     respond_to do |format|
       format.html
