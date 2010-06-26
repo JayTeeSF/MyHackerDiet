8b694b7b4591f969f943f6f425647b8444d22736
Jon <jon@digital-drip.com>
2010-06-26 10:41:43 -0400

    Make user changes

--- app/controllers/withings_log_controller.rb
+++ app/controllers/withings_log_controller.rb
@@ -1,7 +1,7 @@
 class WithingsLogController < ApplicationController
   def index
-    @logs = WithingsLog.find_all_by_userid(current_user.user_option.withings_userid, :order => 'sdate DESC')
-    @withings_logs = Withings.find_all_by_userid(current_user.user_option.withings_userid, :order => 'rec_date DESC')
+    @logs = WithingsLog.find_all_by_userid(current_user.withings_userid, :order => 'sdate DESC')
+    @withings_logs = Withings.find_all_by_userid(current_user.withings_userid, :order => 'rec_date DESC')
 
     respond_to do |format|
       format.html
