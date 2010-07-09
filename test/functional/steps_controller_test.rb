75db87ffb801f1d0c153785325e18dc566a32590
Jon <jon@digital-drip.com>
2010-07-09 14:06:28 -0400

    Refactor tests

--- test/functional/steps_controller_test.rb
+++ test/functional/steps_controller_test.rb
@@ -3,8 +3,16 @@ require 'test_helper'
 class StepsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-  test "should get index" do
+  def setup
     sign_in :user, @jon
+  end
+
+  def teardown
+    sign_out @jon
+  end
+
+
+  test "should get index" do
 
     get :index
     assert_response :success
@@ -12,14 +20,12 @@ class StepsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
-    sign_in :user, @jon
 
     get :new
     assert_response :success
   end
 
   test "should create step" do
-    sign_in :user, @jon
 
     assert_difference('Step.count') do
       post :create, :step => { }
@@ -29,28 +35,24 @@ class StepsControllerTest < ActionController::TestCase
   end
 
   test "should show step" do
-    sign_in :user, @jon
 
     get :show, :id => steps(:one).to_param
     assert_response :success
   end
 
   test "should get edit" do
-    sign_in :user, @jon
 
     get :edit, :id => steps(:one).to_param
     assert_response :success
   end
 
   test "should update step" do
-    sign_in :user, @jon
 
     put :update, :id => steps(:one).to_param, :step => { }
     assert_redirected_to steps_path
   end
 
   test "should destroy step" do
-    sign_in :user, @jon
 
     assert_difference('Step.count', -1) do
       delete :destroy, :id => steps(:one).to_param
--- test/functional/weights_controller_test.rb
+++ test/functional/weights_controller_test.rb
@@ -3,8 +3,15 @@ require 'test_helper'
 class WeightsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-  test "should get index" do
+  def setup
     sign_in :user, @jon
+  end
+
+  def teardown
+    sign_out @jon
+  end
+
+  test "should get index" do
 
     get :index
     assert_response :success
@@ -12,13 +19,11 @@ class WeightsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
-    sign_in :user, @jon
     get :new
     assert_response :success
   end
 
   test "should create weight" do
-    sign_in :user, @jon
     assert_difference('Weight.count') do
       post :create, :weight => { }
     end
@@ -27,25 +32,21 @@ class WeightsControllerTest < ActionController::TestCase
   end
 
   test "should show weight" do
-    sign_in :user, @jon
     get :show, :id => weights(:one).to_param
     assert_response :success
   end
 
   test "should get edit" do
-    sign_in :user, @jon
     get :edit, :id => weights(:one).to_param
     assert_response :success
   end
 
   test "should update weight" do
-    sign_in :user, @jon
     put :update, :id => weights(:one).to_param, :weight => { }
     assert_redirected_to weights_path
   end
 
   test "should destroy weight" do
-    sign_in :user, @jon
     assert_difference('Weight.count', -1) do
       delete :destroy, :id => weights(:one).to_param
     end
