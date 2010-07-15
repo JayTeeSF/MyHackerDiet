6a8ba95e5ddb2f1956cbc57954610bf528fb1221
Jon <jon@digital-drip.com>
2010-07-14 20:19:54 -0400

    Only show steps for the currently logged in user in home stats

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -23,7 +23,7 @@ class StaticController < ApplicationController
       begin
         @steps_7_days = Step.average(:steps, :conditions => ['user_id = ?', current_user.id], :limit => 7, :order => 'rec_date DESC')
 
-        steps_months_sql = 'select year(rec_date) as year, month(rec_date) as month, sum(steps) as steps, sum(mod_steps) as mod_steps, sum(mod_min) as mod_min from steps where month(rec_date) is not null group by year(rec_date), month(rec_date) limit 365'
+        steps_months_sql = "select year(rec_date) as year, month(rec_date) as month, sum(steps) as steps, sum(mod_steps) as mod_steps, sum(mod_min) as mod_min from steps where user_id = #{current_user.id} and month(rec_date) is not null group by year(rec_date), month(rec_date) limit 365"
         @steps_months = Step.connection.select_all(steps_months_sql)
       rescue
         @steps_7_days = 0
