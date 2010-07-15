60231ab092885a652ee144bebf8ce327f00e6a85
Jon <jon@digital-drip.com>
2010-07-14 21:00:02 -0400

    Enable email confirmation for new accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
-  devise :registerable, :database_authenticatable, :recoverable,
+  devise :registerable, :database_authenticatable, :recoverable, :confirmable,
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
