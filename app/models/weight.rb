62e038c2b70ad73a91e432a140814e964b4d0f8a
Jon <jon@digital-drip.com>
2010-07-19 16:15:46 -0400

    Fill weight gaps when entering non-consecutive days

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,6 +1,6 @@
 class Weight < ActiveRecord::Base
   belongs_to :user
-  #before_save :calc_avg_weight
+  before_save :fill_weight_gaps
 
   def bmi
     begin
@@ -22,10 +22,33 @@ class Weight < ActiveRecord::Base
     return (rmr * 1.2).round(2)
   end
 
+  def fill_weight_gaps
+    max_rec = Weight.find_by_user_id(user_id, :limit => 1, :order => 'rec_date DESC')
+
+    if max_rec == nil || (max_rec.rec_date - rec_date).abs == 1
+      return
+    end
+
+    weight_diff = weight - max_rec.weight
+    days_between = (rec_date - max_rec.rec_date).to_i
+    weight_per_day = (weight_diff / days_between).to_f
+
+    (1..(days_between-1)).each do |day|
+      filler = Weight.new
+      filler.user_id = user_id
+      filler.rec_date = max_rec.rec_date + day
+      filler.weight = max_rec.weight + (weight_per_day * day)
+      filler.manual = 0
+      filler.send(:create_without_callbacks)
+      filler.calc_avg_weight
+    end
+
+  end
+
   def calc_avg_weight
     datapool = Weight.find_all_by_user_id(user_id, :limit => 20, :conditions => ["rec_date <= ?", rec_date], :order => 'rec_date DESC')
     self[:avg_weight] = averageweight datapool
-    self.save
+    self.send(:update_without_callbacks)
   end
 
   private
