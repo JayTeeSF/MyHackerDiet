db02392c3865fb1b7705940f39d2f190c81ddb6d
Jon <jon@digital-drip.com>
2010-07-24 14:56:54 -0400

    Update schema from previous updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,6 @@
 
 ActiveRecord::Schema.define(:version => 20100720154242) do
 
-  create_table "posts", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "sessions", :force => true do |t|
-    t.integer  "person_id"
-    t.string   "ip_address"
-    t.string   "path"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "steps", :force => true do |t|
     t.date     "rec_date"
     t.integer  "steps"
@@ -70,17 +54,17 @@ ActiveRecord::Schema.define(:version => 20100720154242) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.decimal  "weight",     :precision => 12, :scale => 2
-    t.decimal  "bodyfat",    :precision => 4,  :scale => 2
-    t.decimal  "avg_weight", :precision => 12, :scale => 2
+    t.decimal  "weight",     :precision => 5, :scale => 2
+    t.decimal  "bodyfat",    :precision => 5, :scale => 2
+    t.decimal  "avg_weight", :precision => 5, :scale => 2
     t.integer  "rec_type"
   end
 
   create_table "withings", :force => true do |t|
     t.integer  "userid"
     t.datetime "rec_date"
-    t.decimal  "weight",     :precision => 12, :scale => 2
-    t.decimal  "bodyfat",    :precision => 12, :scale => 2
+    t.decimal  "weight",     :precision => 5, :scale => 2
+    t.decimal  "bodyfat",    :precision => 5, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
