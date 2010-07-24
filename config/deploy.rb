267a6dbeba43669237c806be80762f46a4e5a2c0
Jon <jon@digital-drip.com>
2010-07-24 15:14:52 -0400

    Add deploy with staging support

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,29 @@
-set :application, "mhd"
 set :repository,  "jon@horder.digital-drip.com:/home/jon/git/myhackerdiet.git"
-set :deploy_to, "/srv/#{application}"
+set :application, "mhd"
 
 set :scm, :git
 set :user, 'jon'
 set :branch, 'master'
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "myhackerdiet.com"                          # Your HTTP server, Apache/etc
-role :app, "myhackerdiet.com"                          # This may be the same as your `Web` server
-role :db,  "myhackerdiet.com", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+if ENV['DEPLOY'] == 'production'
+  puts 'PRODUCTION DEPLOY'
+  set :deploy_to, "/srv/#{application}"
+
+  role :web, "myhackerdiet.com"                          # Your HTTP server, Apache/etc
+  role :app, "myhackerdiet.com"                          # This may be the same as your `Web` server
+  role :db,  "myhackerdiet.com", :primary => true # This is where Rails migrations will run
+elsif ENV['DEPLOY'] == 'staging'
+  puts 'STAGING DEPLOY'
+  set :deploy_to, "/srv/rails/#{application}"
+
+  role :web, "97.107.129.119"                          # Your HTTP server, Apache/etc
+  role :app, "97.107.129.119"                          # This may be the same as your `Web` server
+  role :db,  "97.107.129.119", :primary => true # This is where Rails migrations will run
+else
+  fail 'ERROR!  No Deploy Environment Specified'
+end
+
+
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
