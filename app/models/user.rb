39d4a9072544072b5dfa67f48261516f4d183402
Jon <jon@digital-drip.com>
2010-09-30 19:11:25 -0400

    Add user part of weights relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :weights
+
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
   devise :registerable, :database_authenticatable, :recoverable, :confirmable,
